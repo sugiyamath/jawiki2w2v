bd5af06060d7dbd89e4bbda53f5fcd5f613d636a
root <shun.sugiyama@jp21.com>
2019-02-20 10:17:45 +0900

    update

--- t82w.py
+++ t82w.py
@@ -6,5 +6,5 @@ import multiprocessing
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     sentences = word2vec.Text8Corpus('./wiki_fixed.text8')
-    model = word2vec.Word2Vec(sentences, size=200)
-    model.save("word2vec.model", size=200, workers=multiprocessing.cpu_count())
+    model = word2vec.Word2Vec(sentences, size=200, workers=multiprocessing.cpu_count())
+    model.save("word2vec.model")
